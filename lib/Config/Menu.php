f7eed0e00b2217015d9c4bf41bfebbb18d6b8f60
Rostislav Wolny <rosta@mailpoet.com>
2019-03-14 15:44:36 +0100

    Add empty woo commerce import list page
    [MAILPOET-1732]

--- lib/Config/Menu.php
+++ lib/Config/Menu.php
@@ -335,6 +335,19 @@ class Menu {
       )
     );
 
+    // WooCommerce List Import
+    $this->wp->addSubmenuPage(
+      true,
+      $this->setPageTitle($this->wp->__('WooCommerce List Import', 'mailpoet')),
+      $this->wp->__('WooCommerce List Import', 'mailpoet'),
+      AccessControl::PERMISSION_ACCESS_PLUGIN_ADMIN,
+      'mailpoet-woocommerce-list-import',
+      array(
+        $this,
+        'wooCommerceListImport'
+      )
+    );
+
     // Update page
     $this->wp->addSubmenuPage(
       true,
@@ -387,6 +400,14 @@ class Menu {
       'reply_to' => $this->settings->get('reply_to'),
     ];
     $this->displayPage('welcome_wizard.html', $data);
+  }
+
+  function wooCommerceListImport() {
+    if ((bool)(defined('DOING_AJAX') && DOING_AJAX)) return;
+    $data = [
+
+    ];
+    $this->displayPage('woocommerce_list_import.html', $data);
   }
 
   function update() {
